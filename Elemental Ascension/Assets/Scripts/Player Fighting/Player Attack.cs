a18d3ac88c7fe687d593e681ca5428b89496582e
Brady Lin <linbrady16877@gmail.com>
2024-02-21 16:32:46 -0800

    Added Parry and Attack hitboxes. Connected katana to player.
    Left click to attack / Right click to parry

---
+++ Elemental Ascension/Assets/Scripts/Player Fighting/Player Attack.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    public GameObject HitBox; // Set Hitbox GameObject
+    public GameObject ParryHitbox; // Set ParryHitbox GameObject
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            StartCoroutine(ActivateHitbox());
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            StartCoroutine(ActivateParryHitbox());
+        }
+    }
+
+    IEnumerator ActivateHitbox()
+    {
+        HitBox.SetActive(true);
+        yield return new WaitForSeconds(1f);
+        HitBox.SetActive(false);
+    }
+
+    IEnumerator ActivateParryHitbox()
+    {
+        ParryHitbox.SetActive(true);
+        yield return new WaitForSeconds(0.2f);
+        ParryHitbox.SetActive(false);
+    }
+    
+}
