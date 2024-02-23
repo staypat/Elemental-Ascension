f5947a6cf065ca76b08da42fbd76bc1a18a15d5f
staypat <phhu@ucsc.edu>
2024-02-22 19:43:13 -0800

    Updated Text UI and projectile with damage scripts

--- Elemental Ascension/Assets/Scripts/Player Fighting/Player Health.cs
+++ Elemental Ascension/Assets/Scripts/Player Fighting/Player Health.cs
@@ -1,15 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
     // Start is called before the first frame update
     public int maxHealth = 30;
     private int currentHealth;
+    public TMP_Text healthText;
     void Start()
     {
         currentHealth = maxHealth;
+        updateHealthText();
     }
 
     // Update is called once per frame
@@ -20,9 +24,14 @@ public class PlayerHealth : MonoBehaviour
     public void takeDamage(int damage)
     {
         currentHealth -= damage;
+        updateHealthText();
         if(currentHealth <= 0)
         {
             this.gameObject.SetActive(false);
         }
+    }
+    public void updateHealthText()
+    {
+        healthText.text = currentHealth.ToString() + "/" + maxHealth.ToString();
     }
 }
