5803d586bf713feb8342c8f82229954bfecc691b
Frank Shi <frshi@ucsc.edu>
2024-02-22 21:04:44 -0800

    increased parry window and larger parry box

--- Elemental Ascension/Assets/Scripts/Player Fighting/Player Attack.cs
+++ Elemental Ascension/Assets/Scripts/Player Fighting/Player Attack.cs
@@ -52,7 +52,7 @@ public class PlayerAttack : MonoBehaviour
             canParry = false;
             canAttack = false;
             ParryHitbox.SetActive(true);
-            yield return new WaitForSeconds(0.2f);
+            yield return new WaitForSeconds(0.5f);
             canParry = true;
             canAttack = true;
             ParryHitbox.SetActive(false);
