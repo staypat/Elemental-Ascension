a47a33b75612d08f5402498d2041653f38a284e5
Brady Lin <linbrady16877@gmail.com>
2024-03-08 19:23:31 -0800

    Added parry animation
    Gonna revamp the animation script

--- Elemental Ascension/Assets/Scripts/Animation Scripts/WeaponAnimationScript.cs
+++ Elemental Ascension/Assets/Scripts/Animation Scripts/WeaponAnimationScript.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,38 +10,97 @@ public class WeaponAnimationScript : MonoBehaviour
     public Animation anim;
     public AnimationClip idleClip;
     public AnimationClip attackClip;
+    public AnimationClip parryClip;
     bool idle = true;
     bool isSwinging = false;
+    bool isParrying = false;
     // Start is called before the first frame update
     void Start()
     {
         anim = GetComponent<Animation>();
         anim.AddClip(idleClip, "Idle");
         anim.AddClip(attackClip, "Weapon Swing");
+        anim.AddClip(parryClip, "Parry");
     }
 
     // Update is called once per frame
     void Update()
     {
-        // If the player presses the left mouse button, play the attack animation
-        if (Input.GetMouseButtonDown(0))
+        if (idle)
         {
-            if (idle)
-            {
-                GetComponent<Animation>().Stop("Idle");
-                GetComponent<Animation>().Play("Weapon Swing");
-                idle = false;
-                isSwinging = true;
-            }
+            anim.Play("Idle");
         }
 
-        // If the swing animation has finished playing, set isSwinging to false
-        if (!GetComponent<Animation>().IsPlaying("Weapon Swing")) // Add this line
+        if (isSwinging)
         {
-            isSwinging = false; // Add this line
-            idle = true;
-            GetComponent<Animation>().Stop("Weapon Swing");
-            GetComponent<Animation>().Play("Idle");
+            anim.Play("Weapon Swing");
         }
+        
+        if (isParrying)
+        {
+            anim.Play("Parry");
+        }
+
+        // ----- Attack Animation START -----
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                if (idle)
+                {
+                      
+
+                }
+            }
+
+            // // If the player presses the left mouse button, play the attack animation
+            // if (Input.GetMouseButtonDown(0))
+            // {
+            //     if (idle)
+            //     {
+            //         GetComponent<Animation>().Stop("Idle");
+            //         GetComponent<Animation>().Play("Weapon Swing");
+            //         idle = false;
+            //         isSwinging = true;
+            //         Debug.Log("Swinging Start");
+            //     }
+            // }
+
+            // // If the swing animation has finished playing, set isSwinging to false
+            // if (!GetComponent<Animation>().IsPlaying("Weapon Swing")) // Add this line
+            // {
+            //     isSwinging = false; // Add this line
+            //     idle = true;
+            //     GetComponent<Animation>().Stop("Weapon Swing");
+            //     GetComponent<Animation>().Play("Idle");
+            // }
+
+        // ----- Attack Animation END -----
+
+        // ----- Parry Animation START -----
+
+            // // If the player presses the right mouse button, play the parry animation
+            // if (Input.GetMouseButtonDown(1))
+            // {
+            //     if (idle)
+            //     {
+            //         GetComponent<Animation>().Stop("Idle");
+            //         GetComponent<Animation>().Play("Parry");
+            //         idle = false;
+            //         isParrying = true;
+            //     }
+            // }
+
+            // // If the parry animation has finished playing, set isSwinging to false
+            // if (!GetComponent<Animation>().IsPlaying("Parry")) // Add this line
+            // {
+            //     idle = true;
+            //     isParrying = false;
+            //     GetComponent<Animation>().Stop("Parry");
+            //     GetComponent<Animation>().Play("Idle"); 
+            // }
+
+        // ----- Parry Animation END -----
+
+
     }
 }
