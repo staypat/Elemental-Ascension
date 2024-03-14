2d637b04a5821a26c917e8e8916d37bd504f23d1
Brady Lin <linbrady16877@gmail.com>
2024-03-14 00:25:24 -0700

    added Menu and Credits
    Made a quick menu and credits scene. Credits scene only has "credits:" text.

---
+++ Elemental Ascension/Assets/Scripts/Change Scene.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+
+    // Loading scene
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    // public string sceneToLoad;
+
+    // // Clicking button changes scene
+
+    // public void onClick()
+    // {
+    //     SceneManager.LoadScene(sceneToLoad);
+    // }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
